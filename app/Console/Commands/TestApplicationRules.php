dee50ccf84e2f36355cec5e50ad1ac376218ab17
Akhyar Nasrullah Adilian <akhyarnasrullah8@gmail.com>
2025-08-28 16:55:59 +0700

    feat(TestApplicationRules): Add new console command for testing application rules

---
+++ app/Console/Commands/TestApplicationRules.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestApplicationRules extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-application-rules';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
