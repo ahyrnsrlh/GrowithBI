4faeff0fe0026a6b2c250189cc9bd65ee2cea59c
Akhyar Nasrullah Adilian <akhyarnasrullah8@gmail.com>
2026-01-07 23:37:03 +0700

    style: Add line-clamp property to improve text truncation in logbook components

--- resources/js/Pages/Peserta/Logbooks/Index.vue
+++ resources/js/Pages/Peserta/Logbooks/Index.vue
@@ -536,6 +536,7 @@ const getStatusText = (status) => {
 .line-clamp-3 {
     display: -webkit-box;
     -webkit-line-clamp: 3;
+    line-clamp: 3;
     -webkit-box-orient: vertical;
     overflow: hidden;
 }
