4a05657620d73b50571c3ad2243955cadc810966
Akhyar Nasrullah Adilian <akhyarnasrullah8@gmail.com>
2026-07-07 14:26:55 +0700

    feat: add attendance configuration file and clean up jsconfig compiler options

--- config/attendance.php
+++ config/attendance.php
@@ -2,8 +2,8 @@
 
 return [
     'office' => [
-        'latitude'  => env('OFFICE_LATITUDE',  -5.397140),
-        'longitude' => env('OFFICE_LONGITUDE', 105.266792),
+        'latitude'  => env('OFFICE_LATITUDE',  -5.4194538),
+        'longitude' => env('OFFICE_LONGITUDE', 105.2604370),
     ],
     'radius'           => env('ATTENDANCE_RADIUS_METERS', 500),    // meters
     'gps_accuracy_max' => env('GPS_ACCURACY_MAX_METERS',  50),     // meters
