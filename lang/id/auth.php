12fc1029363b1f3a77687b80880f6076dce4d605
Akhyar Nasrullah Adilian <akhyarnasrullah8@gmail.com>
2026-07-24 07:10:54 +0700

    feat: add Indonesian localization files for authentication, passwords, and validation

---
+++ lang/id/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-baris Bahasa Autentikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentikasi untuk berbagai
+    | pesan yang perlu kita tampilkan kepada pengguna.
+    |
+    */
+
+    'failed' => 'Email atau kata sandi yang Anda masukkan salah.',
+    'password' => 'Kata sandi yang Anda masukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-baris Bahasa Pengingat Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris standar yang cocok dengan alasan
+    | yang diberikan oleh pengelola kata sandi saat upaya pembaruan kata sandi
+    | gagal, seperti token tidak valid atau kata sandi baru tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah berhasil Direset!',
+    'sent' => 'Kami telah mengirimkan tautan reset kata sandi ke email Anda!',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token' => 'Token reset kata sandi ini tidak valid.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+
+];
